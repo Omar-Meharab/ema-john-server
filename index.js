4f9bab76d26631f936eec58f315152fea6b95806
Omar Meharab <omarmeharab81@gmail.com>
2021-04-01 01:58:27 +0600

    server added to heroku

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000
 
+app.get('/', (req, res) =>{
+    res.send("hello world")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
